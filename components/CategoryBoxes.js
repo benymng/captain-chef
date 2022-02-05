077b57282cc372433e273780f1bc59ba89ab45b8
Ben <benymng@gmail.com>
2022-02-05 16:21:35 -0700

    main page created - V1

---
+++ components/CategoryBoxes.js
@@ -0,0 +1,47 @@
+import React from 'react';
+import Image from 'next/image';
+import { FaBeer } from 'react-icons/fa';
+import { RiPlantFill } from 'react-icons/ri';
+import { GiFruitBowl } from 'react-icons/gi';
+import { BsFillCloudHailFill } from 'react-icons/bs';
+
+const CategoryBoxes = ({ search }) => {
+  return (
+    <div className="grid grid-cols-3 gap-3">
+      <CategoryBox text="Carbon Friendly" search={search}>
+        <BsFillCloudHailFill />
+      </CategoryBox>
+      <CategoryBox text="Vegetarian">
+        <RiPlantFill />
+      </CategoryBox>
+      <CategoryBox text="Vegan">
+        <GiFruitBowl />
+      </CategoryBox>
+      {/* <CategoryBox />
+      <CategoryBox /> */}
+    </div>
+  );
+};
+
+const CategoryBox = ({ text, children, search }) => {
+  return (
+    <div>
+      <div
+        onClick={search}
+        className="flex h-16 box md:h-20 border-4 bg-secondary-color rounded-lg border-none hover:bg-accent-color hover:rounded-3xl transition-all duration-300 ease-linear"
+      >
+        <div className="m-auto">
+          {/* clones the react component and adds to it*/}
+          {React.cloneElement(children, {
+            className: 'h-14 w-full text-white mx-auto p-1',
+          })}
+        </div>
+      </div>
+      <p className="text-center font-sans font-bold text-xs md:text-base text-font-color">
+        {text}
+      </p>
+    </div>
+  );
+};
+
+export default CategoryBoxes;
--- pages/index.tsx
+++ pages/index.tsx
@@ -4,14 +4,43 @@ import Link from 'next/link';
 import { useRouter } from 'next/router';
 import { useState } from 'react';
 import styles from '../styles/Home.module.css';
+import SearchBar from '../components/SearchBar';
+import CategoryBoxes from '../components/CategoryBoxes';
+import RecommendedForYou from '../components/RecommendedForYou';
+import Logo from '../public/Logo.svg';
+import Image from 'next/image';
 
 const Home: NextPage = () => {
   const router = useRouter();
 
   const [searchQuery, setSearchQuery] = useState('');
 
+  const search = () => {
+    router.push(`/recipes?q=${searchQuery}`);
+  };
+
   return (
     <div className={styles.container}>
+      <div className="w-full">
+        <h1 className="text-center text-4xl text-font-color font-sans font-bold">
+          Captain Chef
+        </h1>
+      </div>
+      {/* <div className="flex mx-auto">
+        <Image className="text-font-color h-10" src={Logo} />
+      </div> */}
+      <SearchBar
+        search={search}
+        value={searchQuery}
+        setValue={setSearchQuery}
+      />
+      <CategoryBoxes search={search} />
+      <br></br>
+      <p className="pb-3 font-sans font-bold text-font-color text-3xl">
+        Recommended for you:
+      </p>
+      <RecommendedForYou />
+      <br></br>
       <Head>
         <title>Captain Chef</title>
         <meta name="description" content="Recipe app" />
@@ -23,6 +52,11 @@ const Home: NextPage = () => {
           type="text"
           value={searchQuery}
           onChange={(x) => setSearchQuery(x.target.value)}
+        />
+        <SearchBar
+          search={search}
+          value={searchQuery}
+          setValue={setSearchQuery}
         />
         <Link href={`/recipes?q=${searchQuery}`}>
           <button onClick={() => router.push(`/recipes?q=${searchQuery}`)}>
